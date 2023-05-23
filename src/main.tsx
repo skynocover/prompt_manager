dae540d2a054ec6555d4e3e1a11beefc72219b92
EricWu <skynocover@gmail.com>
2023-05-23 15:16:51 +0800

    style: 刪除不必要的引入

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ReactFlowProvider } from 'reactflow';
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './index.css';
